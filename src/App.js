943341851e62292b285416058f93ba6eb2a2498b
Jasmine Elkins <jasmine.elkins@gmail.com>
2022-05-29 14:25:25 -0400

    Cookies are working between app & extension (in window, not popup)

--- src/App.js
+++ src/App.js
@@ -7,45 +7,53 @@ import Login from "./Login";
 function App() {
   const [loggedInUser, setLoggedInUser] = useState(null);
   const [currentTabURL, setCurrentTabURL] = useState("");
+
   const [weeklyReadByDate, setWeeklyReadByDate] = useState();
   const [monthlyReadByDate, setMonthlyReadByDate] = useState();
 
+  const [newsletterID, setNewsletterID] = useState();
+
   useEffect(() => {
-    console.log("Popup opened");
+    // console.log("Popup opened");
 
-    /*global chrome*/
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      //get URL of current tab
-      let current_url = tabs[0].url;
+    // /*global chrome*/
+    // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    //   //get URL of current tab
+    //   let current_url = tabs[0].url;
 
-      console.log(current_url);
-      setCurrentTabURL(current_url);
+    //   console.log(current_url);
+    //   setCurrentTabURL(current_url);
 
-      // GET current User from local storage when popup is opened
-      fetch(`http://localhost:3000/me`)
-        .then((res) => res.json())
-        .then((userObj) => {
-          console.log("Current user: ", userObj);
+    // GET current User from local storage when popup is opened
+    fetch(`/me`)
+      .then((res) => res.json())
+      .then((userObj) => {
+        console.log("Current user: ", userObj);
 
-          if (userObj.id) {
-            console.log("User's name: ", userObj.name);
+        if (userObj.id) {
+          console.log("User's name: ", userObj.name);
 
-            setLoggedInUser(userObj);
-          }
-        })
-        .catch((error) => console.log(error.message));
-    });
+          setLoggedInUser(userObj);
+        }
+      })
+      .catch((error) => console.log(error.message));
+    // });
+
+    console.log("Window opened");
+
+    // for now, open in full window with manual URL:
+    setCurrentTabURL("unicorn");
 
     // default, set read-by date to end of current week:
     const today = new Date();
-    const firstDayOfWeek = new Date(
-      today.setDate(today.getDate() - today.getDay())
-    );
+    // const firstDayOfWeek = new Date(
+    //   today.setDate(today.getDate() - today.getDay())
+    // );
     const lastDayOfWeek = new Date(
       today.setDate(today.getDate() - today.getDay() + 6)
     );
 
-    console.log("Start of the week: ", firstDayOfWeek);
+    // console.log("Start of the week: ", firstDayOfWeek);
     console.log("End of the week: ", lastDayOfWeek);
 
     setWeeklyReadByDate(lastDayOfWeek);
@@ -65,45 +73,111 @@ function App() {
   function handleWeeklyClick() {
     console.log("Button clicked - weekly");
 
-    // POST create new Article with url and user_id
-    fetch(`http://localhost:3000/articles`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        url: currentTabURL,
-        user_id: loggedInUser.id,
-        newsletter_id: 1,
-        read_by_date: weeklyReadByDate,
-      }),
+    const { id } = loggedInUser;
+
+    // first, check if unsent weekly Newsletter exists
+    fetch(`/users/${id}/newsletters`, {
+      method: "GET",
     })
       .then((res) => res.json())
-      .then((newArticlObj) => console.log("Article created: ", newArticlObj))
+      .then((newslettersArray) => {
+        console.log("Newsletter Array: ", newslettersArray); // this is working
+
+        if (newslettersArray > 0) {
+          console.log("not empty");
+          // const unsentNewsletters = newslettersArray.filter(
+          //   (nl) => nl.type === "weekly" && nl.sent === false
+          // );
+          // const firstUnsent = unsentNewsletters[0];
+          // setNewsletterID(firstUnsent.id);
+        } else {
+          console.log("empty");
+          console.log(loggedInUser.id);
+          // create new newsletter
+          // fetch(`http://localhost:3000/newsletters`, {
+          //   method: "POST",
+          //   headers: {
+          //     "Content-Type": "application/json",
+          //     Accept: "application/json",
+          //   },
+          //   body: JSON.stringify({
+          //     type: "weekly",
+          //     publish_date: weeklyReadByDate,
+          //     user_id: loggedInUser.id,
+          //     sent: false,
+          //   }),
+          // })
+          //   .then((res) => res.json())
+          //   .then((newNewsletterObj) => {
+          //     console.log(newNewsletterObj);
+          //     // setNewsletterID(newNewsletterObj.id);
+          //   })
+          //   .catch((error) => console.log(error.message));
+        }
+
+        //  POST create new Article with url and user_id
+        // fetch(`http://localhost:3000/articles`, {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //     Accept: "application/json",
+        //   },
+        //   body: JSON.stringify({
+        //     url: currentTabURL,
+        //     user_id: loggedInUser.id,
+        //     newsletter_id: newsletterID,
+        //     read_by_date: weeklyReadByDate,
+        //   }),
+        // })
+        //   .then((res) => res.json())
+        //   .then((newArticlObj) =>
+        //     console.log("Article created: ", newArticlObj)
+        //   )
+        //   .catch((error) => console.log(error.message));
+      })
       .catch((error) => console.log(error.message));
+
+    // if not, create it
+
+    // POST create new Article with url and user_id
+    //   fetch(`http://localhost:3000/articles`, {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //       Accept: "application/json",
+    //     },
+    //     body: JSON.stringify({
+    //       url: currentTabURL,
+    //       user_id: loggedInUser.id,
+    //       newsletter_id: ,
+    //       read_by_date: weeklyReadByDate,
+    //     }),
+    //   })
+    //     .then((res) => res.json())
+    //     .then((newArticlObj) => console.log("Article created: ", newArticlObj))
+    //     .catch((error) => console.log(error.message));
   }
 
   function handleMonthlyClick() {
     console.log("Button clicked - monthly");
 
     // POST create new Article with url and user_id **MONTHLY READ-BY DATE**
-    fetch(`http://localhost:3000/articles`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        url: currentTabURL,
-        user_id: loggedInUser.id,
-        newsletter_id: 1,
-        read_by_date: monthlyReadByDate,
-      }),
-    })
-      .then((res) => res.json())
-      .then((newArticlObj) => console.log("Article created: ", newArticlObj))
-      .catch((error) => console.log(error.message));
+    // fetch(`http://localhost:3000/articles`, {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Accept: "application/json",
+    //   },
+    //   body: JSON.stringify({
+    //     url: currentTabURL,
+    //     user_id: loggedInUser.id,
+    //     newsletter_id: 1,
+    //     read_by_date: monthlyReadByDate,
+    //   }),
+    // })
+    //   .then((res) => res.json())
+    //   .then((newArticlObj) => console.log("Article created: ", newArticlObj))
+    //   .catch((error) => console.log(error.message));
   }
 
   return (
@@ -117,7 +191,7 @@ function App() {
           <div className="innerContainer">
             <h2>Hi, {loggedInUser.name}</h2>
 
-            <div class="imageContainer">
+            <div className="imageContainer">
               <button className="addBtn" onClick={handleWeeklyClick}>
                 Add to Weekly Newsletter
               </button>
--- src/Login.js
+++ src/Login.js
@@ -31,7 +31,7 @@ function Login({ loggedInUser, setLoggedInUser }) {
     e.preventDefault();
 
     // POST request: log in user by creating a session
-    fetch(`http://localhost:3000/login`, {
+    fetch(`/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
